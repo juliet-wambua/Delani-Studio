ee0af2654bc1abf04e83c66ecbf05808bbd104f2
juliet-wambua <julienea289@gmail.com>
2019-06-10 11:07:02 +0300

    alerts

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,18 @@ $(document).ready(function(){
 
   $(document).ready(function(){
     $(".btn").click(function(){
-      alert("Hello!!Thank you for submitting, we will get to you soon.");
+      alert();
     });
   });
+  $("#submittion").submit(function(event){
+       var name = $("input#name").val();
+       var email =$("input#email").val();
+       var message=$("input#message").val();
+       if(name && email && message != " " ){
+           alert("Thank you for your submitting your details. We will get back to you soon.");
+       }
+       else{
+           alert("Kindly fill in your details in the inputs provided");
+       }
+       event.preventDefault();
+      });
