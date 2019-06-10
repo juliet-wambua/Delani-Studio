bfd6f8b888fea80b069e63158a7cab50a81a7686
juliet-wambua <julienea289@gmail.com>
2019-06-10 10:38:38 +0300

    Add README.md

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $("#hide1").click(function(){
+    $("#design").toggle();
+   });
+  });
+
+  $(document).ready(function(){
+    $("#hide2").click(function(){
+      $("#development").toggle();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#hide3").click(function(){
+      $("#product").toggle();
+    });
+  });
+
+  $(document).ready(function(){
+    $(".btn").click(function(){
+      alert("Hello!!Thank you for submitting, we will get to you soon.");
+    });
+  });
