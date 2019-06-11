a36336bb739e6589a768df19ce39e85557e50d2d
juliet-wambua <julienea289@gmail.com>
2019-06-11 18:01:43 +0300

    Add chages in all stylings

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,89 @@
 $(document).ready(function(){
-  $("#hide1").click(function(){
-    $("#design").toggle();
+  $(".hide1").click(function(){
+     $(".design").show();
+      $(".hide1").hide();
+    });
+   $(".design").click(function(){
+      $(".hide1").show();
+     $(".design").hide();
+     });
    });
-  });
-
-  $(document).ready(function(){
-    $("#hide2").click(function(){
-      $("#development").toggle();
+$(document).ready(function(){
+ $(".hide2").click(function(){
+    $(".development").show();
+     $(".hide2").hide();
+   });
+  $(".development").click(function(){
+     $(".hide2").show();
+    $(".development").hide();
     });
   });
 
   $(document).ready(function(){
-    $("#hide3").click(function(){
-      $("#product").toggle();
-    });
-  });
+    $(".hide3").click(function(){
+       $(".product").show();
+        $(".hide3").hide();
+      });
+     $(".product").click(function(){
+        $(".hide3").show();
+       $(".product").hide();
+       });
+     });
+
 
   $(document).ready(function(){
     $(".btn").click(function(){
       alert();
+    });
+  });
+  $(document).ready(function(){
+    $(".01").mouseenter(function(){
+      $(".image-1").show();
+    });
+    $(".01").mouseleave(function(){
+      $(".image-1").hide();
+    });
+    $(".02").mouseenter(function(){
+      $(".image-2").show();
+    });
+    $(".02").mouseleave(function(){
+      $(".image-2").hide();
+    });
+    $(".03").mouseenter(function(){
+      $(".image-3").show();
+    });
+    $(".03").mouseleave(function(){
+      $(".image-3").hide();
+    });
+    $(".04").mouseenter(function(){
+      $(".image-4").show();
+    });
+    $(".04").mouseleave(function(){
+      $(".image-4").hide();
+    });
+    $(".05").mouseenter(function(){
+      $(".image-5").show();
+    });
+    $(".05").mouseleave(function(){
+      $(".image-5").hide();
+    });
+    $(".06").mouseenter(function(){
+      $(".image-6").show();
+    });
+    $(".06").mouseleave(function(){
+      $(".image-6").hide();
+    });
+    $(".07").mouseenter(function(){
+      $(".image-7").show();
+    });
+    $(".07").mouseleave(function(){
+      $(".image-7").hide();
+    });
+    $(".08").mouseenter(function(){
+      $(".image-8").show();
+    });
+    $(".08").mouseleave(function(){
+      $(".image-8").hide();
     });
   });
   $("#submittion").submit(function(event){
@@ -33,3 +98,4 @@ $(document).ready(function(){
        }
        event.preventDefault();
       });
+
